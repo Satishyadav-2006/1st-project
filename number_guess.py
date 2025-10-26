795e2b3774f6271ce03a03174266f7d287795164
Satish Yadav <mkrishnan69928@gmail.com>
2025-10-26 20:33:08 +0530

    number guessing game

--- number_guess.py
+++ number_guess.py
@@ -1,1 +1,11 @@
-
+import random
+n = random.raandint(1,100)
+guess = 0
+while guess != n:
+  guess = int(input("Guess a number between 1 to 100"))
+  if n < guess:
+    print("your guess is too high...")
+  elif n > guess:
+    print("your guess is too low...")
+  else:
+    print("PERFECT GUESS!")
